4d5152efb639f50c55eef1a85648d8416ecf0ba0
k-joseph <kaweesi@jembi.org>
2021-02-04 19:58:15 +0300

    enable tests on travis

--- travis_script.sh
+++ travis_script.sh
@@ -23,5 +23,5 @@ cd ../../../ui-tests
 mvn verify -Pci -Dwebapp.url=http://localhost:8080/openmrs -Dsaucelabs.hub.url=localhost:4445 -DbuildNumber=$TRAVIS_BUILD_NUMBER -Dbranch=$TRAVIS_BRANCH -DsaucelabsTunnel=$TRAVIS_JOB_NUMBER -Dmaven.junit.usefile=false
 
 else
-mvn clean install -U -q
+mvn clean install -DskipTests=false -U -q
 fi
